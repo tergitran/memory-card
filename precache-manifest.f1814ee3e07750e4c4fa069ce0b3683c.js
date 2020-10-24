469e471e424bd6206c2997d6350190430e2073b6
tergitran <tergitran@gmail.com>
2020-10-24 13:43:25 +0700

    Updates

---
+++ precache-manifest.f1814ee3e07750e4c4fa069ce0b3683c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867d74b3c6c132d51bd07302cd80855a",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "a60b3706ee72977712ad",
+    "url": "/memory-card/static/css/main.36cc6e97.chunk.css"
+  },
+  {
+    "revision": "0be02312647e4ab34121",
+    "url": "/memory-card/static/js/2.ec26851a.chunk.js"
+  },
+  {
+    "revision": "d60db9bf004e045c1b5e484daae9d8c0",
+    "url": "/memory-card/static/js/2.ec26851a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60b3706ee72977712ad",
+    "url": "/memory-card/static/js/main.d66f1ec3.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  }
+]);
